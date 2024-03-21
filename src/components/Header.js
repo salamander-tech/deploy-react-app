c4e32279f7c9003aa9c30c7d423ad8393bf3b942
salamander-tech <jean.h.bidi@gmail.com>
2024-03-21 12:37:25 +0100

    Load data fromjson forvaruious components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import menuLinksData from './data/menu_links.json';
+
 function Header() {
     return (
         <header id="intro">
@@ -26,31 +28,15 @@ function Header() {
                         </a>
                     </div>
                     <ul>
-                        <li>
-                            <a className="icon info" href="#hotelinfo">
-                                <span>info</span>
+
+                    {menuLinksData.map((link, index) => (
+                        <li key={index}>
+                            <a className={`icon ${link.class}`} href={link.href}>
+                                <span>{link.text}</span>
                             </a>
                         </li>
-                        <li>
-                            <a className="icon rooms" href="#rooms">
-                                <span>rooms</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="icon dining" href="#dining">
-                                <span>dining</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="icon events" href="#events">
-                                <span>events</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a className="icon attractions" href="#attractions">
-                                <span>attractions</span>
-                            </a>
-                        </li>
+                    ))}
+                          
                     </ul>
                 </div>
             </nav>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import welcome_data from './data/welcome_data.json';
 
 const Welcome = () => {
 
@@ -6,21 +7,20 @@ const Welcome = () => {
         <div className="scene" id="welcome">
         <article className="content">
           <div className="gallery">
-            <img src="https://landonhotel.com/images/hotel/intro_room.jpg" alt="Intro Gallery Room Sample Pictures" />
-            <img src="https://landonhotel.com/images/hotel/intro_pool.jpg" alt="Intro Gallery Pool Sample Pictures" />
-            <img src="https://landonhotel.com/images/hotel/intro_dining.jpg" alt="Intro Gallery Dining Sample Pictures" />
-            <img src="https://landonhotel.com/images/hotel/intro_attractions.jpg" alt="Intro Gallery Attractions Sample Pictures" />
-            <img className="hidesm" src="https://landonhotel.com/images/hotel/intro_wedding.jpg" alt="Intro Gallery Dining Sample Pictures" />
-          </div>
+          
+          {
+            welcome_data.map((data, index) => (
+              <img key={index}  className={data.class} src={data.src} alt={data.alt} />
+            ))
+          }
+
+      </div>
           <h1>Welcome to the Landon&nbsp;Hotel</h1>
           <p>The original Landon perseveres after 50 years in the heart of West London. The West End neighborhood has something for everyone—from theater to dining to historic sights. And the not-to-miss Rooftop Cafe is a great place for travelers and locals to engage over drinks, food, and good&nbsp;conversation. &nbsp;To learn more about the Landon Hotel in the West End, browse our website and <a href="files/landon_information_sheet_London.pdf">download our handy information sheet</a>.</p>
         </article>
       </div>
 
     );
-
-    
 }
-
 
 export default Welcome;
