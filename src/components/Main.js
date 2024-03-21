d650933ca91b9dc7ef37f72550f5890ed745113d
salamander-tech <jean.h.bidi@gmail.com>
2024-03-21 03:19:38 +0100

    Add components

---
+++ src/components/Main.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import Welcome from './Welcome';
+import HotelInfo from './HotelInfo';
+
+const Main = () => {  
+
+return (
+  <main id="wrapper">
+  <Welcome/>
+  <HotelInfo/>
+  </main>
+); 
+
+
+}
+
+export default Main;
